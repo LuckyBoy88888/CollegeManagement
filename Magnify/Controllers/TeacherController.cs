2891f842b7d26866d082e0ce6686798f83a0eea7
danny <lovemother320@hotmail.com>
2023-01-20 05:41:11 +0900

    Update Teacher

--- Magnify/Controllers/TeacherController.cs
+++ Magnify/Controllers/TeacherController.cs
@@ -57,85 +57,65 @@ namespace Magnify.Controllers
         // GET: Teacher/Create
         public ActionResult Create()
         {
-            ViewBag.SubjectID = new SelectList(db.Subjects, "SubjectID", "Title");
+            //ViewBag.SubjectID = new SelectList(db.Subjects, "SubjectID", "Title");
             return View();
         }
 
-        // POST: Teacher/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "TeacherID,FirstName,LastName,SubjectID")] Teacher teacher)
+        // GET: All Subjects
+        [HttpGet]
+        public JsonResult GetAllSubject()
         {
-            if (ModelState.IsValid)
-            {
-                db.Teachers.Add(teacher);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.SubjectID = new SelectList(db.Subjects, "SubjectID", "Title", teacher.SubjectID);
-            return View(teacher);
+            var subjects = db.Subjects.ToList();
+            var data = (from host in subjects
+                        select new
+                        {
+                            SubjectID = host.SubjectID,
+                            Title = host.Title
+                        }).ToArray();
+            return Json(data, JsonRequestBehavior.AllowGet);
         }
 
-        // GET: Teacher/Edit/5
-        public ActionResult Edit(int? id)
+        public JsonResult Insert([Bind(Include = "TeacherID,FirstName,LastName,SubjectID")] Teacher teacher)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
+            db.Teachers.Add(teacher);
+            db.SaveChanges();
+
+            return Json(teacher, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult GetByID(int id)
+        {
             Teacher teacher = db.Teachers.Find(id);
-            if (teacher == null)
+            var data = new
             {
-                return HttpNotFound();
-            }
-            ViewBag.SubjectID = new SelectList(db.Subjects, "SubjectID", "Title", teacher.SubjectID);
-            return View(teacher);
+                TeacherID = teacher.TeacherID,
+                FirstName = teacher.FirstName,
+                LastName = teacher.LastName,
+                SubjectID = teacher.SubjectID
+            };
+            return Json(data, JsonRequestBehavior.AllowGet);
         }
 
         // POST: Teacher/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "TeacherID,FirstName,LastName,SubjectID")] Teacher teacher)
+        public JsonResult Update([Bind(Include = "TeacherID,FirstName,LastName,SubjectID")] Teacher teacher)
         {
-            if (ModelState.IsValid)
-            {
-                db.Entry(teacher).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.SubjectID = new SelectList(db.Subjects, "SubjectID", "Title", teacher.SubjectID);
-            return View(teacher);
-        }
-
-        // GET: Teacher/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Teacher teacher = db.Teachers.Find(id);
-            if (teacher == null)
-            {
-                return HttpNotFound();
-            }
-            return View(teacher);
+            db.Entry(teacher).State = EntityState.Modified;
+            db.SaveChanges();
+            return Json(teacher, JsonRequestBehavior.AllowGet);
         }
 
         // POST: Teacher/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        [HttpPost]
+        public JsonResult Delete(int id)
         {
             Teacher teacher = db.Teachers.Find(id);
             db.Teachers.Remove(teacher);
             db.SaveChanges();
-            return RedirectToAction("Index");
+            return Json(teacher, JsonRequestBehavior.AllowGet);
         }
 
         protected override void Dispose(bool disposing)
--- Magnify/ScriptsNg/TeacherController.js
+++ Magnify/ScriptsNg/TeacherController.js
@@ -2,7 +2,12 @@
     .controller("TeacherController", function ($scope, $http, $location, $window) {
         $scope.message = '';
         $scope.ListTeacher = null;
+        $scope.ListSubject = null;
+        $scope.custModel = {};
+        $scope.result = "color-default";
+        $scope.isViewLoading = false;
 
+        getallSubject();
         getallData();
 
         //******* Get All Course ********
@@ -15,7 +20,91 @@
                 $scope.ListTeacher = response.data;
             }, function (error) {
                 $scope.message = 'Unexpected Error while loading data!!';
+                $scope.result = "color-red";
                 console.log($scope.message);
             });
+        }
+
+        //******* Get All Subject ********
+        function getallSubject() {
+            //debugger;
+            $http({
+                method: 'GET',
+                url: '/Teacher/GetAllSubject'
+            }).then(function (response) {
+                $scope.ListSubject = response.data;
+            }, function (error) {
+                $scope.message = 'Unexpected Error while loading data!!';
+                $scope.result = "color-red";
+                console.log($scope.message);
+            });
+        }
+
+        //******* Save Teacher ********
+        $scope.saveTeacher = function () {
+            $scope.isViewLoading = true;
+
+            $http({
+                method: 'POST',
+                url: '/Teacher/Insert',
+                data: $scope.custModel
+            }).then(function (response) {
+                $scope.message = "Form data Saved!";
+                $scope.result = "color-green";
+                getallData();
+                $scope.custModel = {};
+            }, function (error) {
+                $scope.message = 'Unexpected Error while saving data!!' + error;
+                $scope.result = 'color-red';
+            })
+
+            $scope.isViewLoading = false;
+        }
+
+        $scope.deleteTeacher = function (custModel) {
+            var IsConf = confirm("You are about to delete " + custModel.FirstName + '. Are you sure?');
+            if (IsConf) {
+                $http({
+                    method: 'POST',
+                    url: '/Teacher/Delete/' + custModel.TeacherID
+                }).then(function (response) {
+                    $scope.message = custModel.FirstName + ' deleted from record!!';
+                    $scope.result = 'color-green';
+                    getallData();
+                }, function (error) {
+                    $scope.message = 'Error on deleting Record!';
+                    $scope.result = 'color-red';
+                })
+            }
+        }
+
+        $scope.getTeacher = function (custModel) {
+            $http({
+                method: 'GET',
+                url: '/Teacher/GetByID/' + custModel.TeacherID
+            }).then(function (response) {
+                $scope.custModel = response.data;
+                getallData();
+            }, function (error) {
+                $scope.message = 'Unexpected Error while loading data!!';
+                $scope.result = 'color-red';
+            })
+        }
+
+        $scope.updateTeacher = function () {
+            $scope.isViewLoading = true;
+            $http({
+                method: 'POST',
+                url: '/Teacher/Update',
+                data: $scope.custModel
+            }).then(function (response) {
+                $scope.message = "Form data Updated!";
+                $scope.result = 'color-green';
+                getallData();
+            }, function (error) {
+                $scope.message = 'Unexpected Error while updating data!!';
+                $scope.result = 'color-red';
+            })
+            $scope.isViewLoading = false;
         }
     })
